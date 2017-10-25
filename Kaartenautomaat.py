bb07b99328f909511034be66ae02053cdcbb10db
victo <victor.adamse@student.hu.nl>
2017-10-25 11:41:07 +0200

    functie toegevoegd voor ophalen van reisinformatie

--- Kaartenautomaat.py
+++ Kaartenautomaat.py
@@ -4,37 +4,38 @@ import xmltodict
 
 auth_details = ('wesackah@gmail.com', 'aw17-v2gaEXQp1zWCvHXPW-M7lmVFrj1wr05rbTgmyPN0PF7-HDsJg')
 #returnt een lijst met alle vertrektijden als strings
-def vertrekTijd():
-    api_url = 'http://webservices.ns.nl/ns-api-avt?station=ut'
+def vertrekTijd(code):
+    api_url = 'http://webservices.ns.nl/ns-api-avt?station=' + code
     response = requests.get(api_url, auth=auth_details)
     vertrekXML = xmltodict.parse(response.text)
-    vertreklijst = []
+    vertrekLijst = []
     for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
         str = ''
-        str += vertrek['EindBestemming'] + ' '
-        vertrektijd = vertrek['VertrekTijd'] + ' ' # 2016-09-27T18:36:00+0200
-        str += vertrektijd[11:16] + ' ' # 18:36
-        str += vertrek['TreinSoort'] + ' '
-        spoor = vertrek['VertrekSpoor']
-        str += spoor['#text']
+        str += vertrek['EindBestemming'] + ';'
+        vertrekTijd = vertrek['VertrekTijd'] + ';' # 2016-09-27T18:36:00+0200
+        str += vertrekTijd[11:16] + ';' # 18:36
+        str += vertrek['TreinSoort'] + ';'
+        try:
+            str += vertrek['VertrekSpoor']['#text']
+        except:
+            str += 'onbekend'
         # if spoor['@wijziging'] == "True":
         #     str += ' spoorweiziging'
-        vertreklijst.append(str)
-    return vertreklijst
+        vertrekLijst.append(str)
+    return vertrekLijst
 
 #opent een window met de reisinformatie van het huidige station
-def reisinformatie():
-    infoframe = Frame(width=1280, height=720, bg="Yellow")
-    scrollbar = Scrollbar(master=infoframe)
-    listbox = Listbox(infoframe, yscrollcommand=scrollbar.set, height= 25,width= 116,font=('Helvetica',14),bg='yellow')
-    for tijd in vertrekTijd():
-        listbox.insert(END, tijd)
-    listbox.place(x=0,y=0)
-    scrollbar.config(command=listbox.yview)
+def reisInformatie(code):
+    infoFrame = Frame(width=1280, height=720, bg="Yellow")
+    scrollbar = Scrollbar(master=infoFrame,)
+    listBox = Listbox(infoFrame, yscrollcommand=scrollbar.set, height= 25,width= 116,font=('Consolas',14),bg='yellow')
+    for tijd in vertrekTijd(code):
+        listBox.insert(END, makeString(tijd))
+    listBox.place(x=0,y=0)
+    scrollbar.config(command=listBox.yview)
     scrollbar.place(x=1260, y=0)
 
-    back = Button(master=infoframe,
-                  text='terug',
+    back = Button(master=infoFrame,text='terug',
                   bg="Blue",
                   fg="White",
                   activebackground="Blue",
@@ -42,9 +43,26 @@ def reisinformatie():
                   font=('Helvetica', 14, 'bold italic'),
                   width=14,
                   height=2,
-                  command= infoframe.destroy)
+                  command= infoFrame.destroy)
     back.place(x=1000, y=600)
-    infoframe.place(x=0, y=0)
+    infoFrame.place(x=0, y=0)
+
+def reisInfoUtrecht():
+    reisInformatie('ut')
+
+def reisInfoAnder():
+    code = ''
+
+    reisInformatie(code)
+
+#maakt de string geordend door er spaties tussen te zetten
+def makeString(inputString):
+    outputString = ''
+    for word in inputString.split(';'):
+        while len(word) < 27:
+            word += ' '
+        outputString += word
+    return outputString
 
 #stel het TK window in met de goede afmetingen en de buttons en labels van het startscherm
 root = Tk()
@@ -108,7 +126,7 @@ button05 = Button(master= root,
                   font=('Helvetica', 14, 'bold italic'),
                   width=14,
                   height=2,
-                  command=reisinformatie)
+                  command=reisInfoUtrecht)
 button05.place(x=860,y=400)
 button06 = Button(master= root,
                   text='Reisinformatie \n ander station',
@@ -118,6 +136,7 @@ button06 = Button(master= root,
                   activeforeground= "White",
                   font=('Helvetica', 14, 'bold italic'),
                   width=14,
-                  height=2)
+                  height=2,
+                  command=reisInfoAnder)
 button06.place(x=1060,y=400)
 root.mainloop()
