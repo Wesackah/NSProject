f8a9e7878fe1d321d2a20f9de99239f2022be5b4
victo <victor.adamse@student.hu.nl>
2017-10-26 12:03:54 +0200

    applicatie werkt nu en comments zijn goed

--- Kaartenautomaat.py
+++ Kaartenautomaat.py
@@ -2,51 +2,44 @@ from tkinter import *
 import requests
 import xmltodict
 
-#inloggegevens om bij de NS api te kunnen
-inlogGegevens = ('wesackah@gmail.com', 'aw17-v2gaEXQp1zWCvHXPW-M7lmVFrj1wr05rbTgmyPN0PF7-HDsJg')
-response = requests.get('http://webservices.ns.nl/ns-api-stations-v2', auth=inlogGegevens)
-
-#zet alle stationsnamen en de bijhoorende codes in een dictionary
-stationDict = {}
-for station in xmltodict.parse(response.text)['Stations']['Station']:
-    stationDict[station['Namen']['Lang']] = station['Code']
-
 #returnt een lijst met alle vertrektijden als strings
 def vertrekTijd(code):
     api_url = 'http://webservices.ns.nl/ns-api-avt?station=' + code
     response = requests.get(api_url, auth=inlogGegevens)
     vertrekXML = xmltodict.parse(response.text)
-    vertrekLijst = []
+    vertrekList = []
     for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-        str = ''
-        str += vertrek['EindBestemming'] + ';'
-        vertrekTijd = vertrek['VertrekTijd'] + ';' # 2016-09-27T18:36:00+0200
-        str += vertrekTijd[11:16] + ';' # 18:36
+        str = vertrek['EindBestemming'] + ';'
+        str += vertrek['VertrekTijd'][11:16] + ';'
         str += vertrek['TreinSoort'] + ';'
+        #er is niet altijd een spoor bekend bij het XML bestand van de NS. daarom gebruiken we hier een try/except
         try:
             str += vertrek['VertrekSpoor']['#text']
         except:
             str += 'onbekend'
-        vertrekLijst.append(str)
-    return vertrekLijst
+        vertrekList.append(str)
+    return vertrekList
 
+#returnt een lijst met alle vertrektijden als strings
 def storingen(code):
     api_url = 'http://webservices.ns.nl/ns-api-storingen?station=' + code
     response = requests.get(api_url, auth=inlogGegevens)
     storingXML = xmltodict.parse(response.text)
 
     if storingXML['Storingen']['Gepland'] != None:
-        storingList = ['Geplande Storingen']
+        storingList = ['Geplande storingen']
         for storing in storingXML['Storingen']['Gepland']['Storing']:
-            str = ''
-            str += storing['Traject'] + ' '
-            str += storing['Periode']
-            storingList.append(str)
+            storingList.append(storing['Traject'] + ' ' + storing['Periode'])
 
-    # if storingXML['Storingen']['Ongepland'] != None:
-    #     storingList.append('Ongeplande Storingen')
-    #     for storing in storingXML['Storingen']['Ongepland']['Storing']:
-    #         storingList.append(storing['Traject'])
+    if storingXML['Storingen']['Ongepland'] != None:
+        storingList.append('')
+        storingList.append('Ongeplande storingen')
+        if type(storingXML['Storingen']['Ongepland']['Storing']) == list:
+            x = storingXML['Storingen']['Ongepland']['Storing'][0]
+            storingList.append(x['Traject'] + ' ' + x['Reden'])
+        else:
+            x = storingXML['Storingen']['Ongepland']['Storing']
+            storingList.append(x['Traject'] + ' ' + x['Reden'])
 
     return storingList
 
@@ -55,17 +48,20 @@ def reisInformatie(code):
     infoFrame = Frame(width=1280, height=720, bg="Yellow")
     scrollbar = Scrollbar(master=infoFrame) #scrollbar om door de lijst te kunnen scrollen
     listBox = Listbox(infoFrame, yscrollcommand=scrollbar.set, height= 25,width= 128,font=('Consolas',14),bg='yellow')
-    for tijd in vertrekTijd(code): #for loop die de juiste tijden ophaalt via vertrekTijd() en ze daarna in de ListBox zet met de juiste opmaak via makeString()
+    # for loop die de juiste tijden ophaalt via vertrekTijd() en ze daarna in de ListBox zet met de juiste opmaak via makeString()
+    for tijd in vertrekTijd(code):
         listBox.insert(END, makeString(tijd))
     listBox.insert(END,'')
-    for storing in storingen(code): #het zelfde voor de storingen
+    # het zelfde voor de storingen
+    for storing in storingen(code):
         listBox.insert(END, storing)
     listBox.place(x=0,y=25)
     scrollbar.config(command=listBox.yview)
     scrollbar.place(x=1260, y=25)
-    infoLabel = Label(text=makeString('Bestemming;Tijd;Type;Spoor'),font=('Consolas',14,'bold'),bg='yellow',width=108)
+    infoLabel = Label(master= infoFrame, text=makeString('Bestemming;Tijd;Type;Spoor'),font=('Consolas',14,'bold'),bg='yellow',width=108)
     infoLabel.place(x=0,y=0)
-    back = Button(master=infoFrame,text='terug', #back button om terug te gaan naar het hoofdmenu
+    # back button om terug te gaan naar het hoofdmenu
+    back = Button(master=infoFrame,text='terug',
                   bg="Blue",
                   fg="White",
                   activebackground="Blue",
@@ -74,7 +70,7 @@ def reisInformatie(code):
                   width=14,
                   height=2,
                   command= infoFrame.destroy)
-    back.place(x=1000, y=600)
+    back.place(x=1000, y=610)
     infoFrame.place(x=0, y=0)
 
 #roept reisInformatie() aan met de code van Utrecht
@@ -91,7 +87,14 @@ def reisInfoAnder():
                 found = naam
                 break
         if found == '':
-            print('foute invoer')
+            melding = Label(master=selectFrame,
+                            text='geen geldig station',
+                            bg="yellow",
+                            fg="red",
+                            font=('Helvetica', 30),
+                            width=16,
+                            height=1)
+            melding.place(x=300,y=360)
         else:
             reisInformatie(stationDict[found])
             selectFrame.destroy()
@@ -131,6 +134,17 @@ def makeString(inputString):
         outputString += word
     return outputString
 
+
+
+#inloggegevens om bij de NS api te kunnen
+inlogGegevens = ('wesackah@gmail.com', 'aw17-v2gaEXQp1zWCvHXPW-M7lmVFrj1wr05rbTgmyPN0PF7-HDsJg')
+response = requests.get('http://webservices.ns.nl/ns-api-stations-v2', auth=inlogGegevens)
+
+#zet alle stationsnamen en de bijhoorende codes in een dictionary
+stationDict = {}
+for station in xmltodict.parse(response.text)['Stations']['Station']:
+    stationDict[station['Namen']['Lang']] = station['Code']
+
 #stel het TK window in met de goede afmetingen en de buttons en labels van het startscherm
 root = Tk()
 root.geometry("1280x720")
@@ -147,7 +161,7 @@ title = Label(master= root,
 title.pack()
 #alle buttons die geen functie hebben
 button01 = Button(master= root,
-                  text='Ik wil naar \n Amsterdam',
+                  text='Ik wil naar \nAmsterdam',
                   bg="Blue",
                   fg= "White",
                   activebackground= "Blue",
@@ -157,7 +171,7 @@ button01 = Button(master= root,
                   height= 2)
 button01.place(x=60,y=400)
 button02 = Button(master= root,
-                  text='Kopen \n los kaartje',
+                  text='Kopen \nlos kaartje',
                   bg="Blue",
                   fg= "White",
                   activebackground= "Blue",
@@ -167,7 +181,7 @@ button02 = Button(master= root,
                   height=2)
 button02.place(x=260,y=400)
 button03 = Button(master= root,
-                  text='Kopen \n OV-Chipkaart',
+                  text='Kopen \nOV-Chipkaart',
                   bg="Blue",
                   fg= "White",
                   activebackground= "Blue",
@@ -177,7 +191,7 @@ button03 = Button(master= root,
                   height=2)
 button03.place(x=460,y=400)
 button04 = Button(master= root,
-                  text='ik wil naar \n het buitenland',
+                  text='ik wil naar \nhet buitenland',
                   bg="Blue",
                   fg= "White",
                   activebackground= "Blue",
@@ -189,7 +203,7 @@ button04.place(x=660,y=400)
 
 #button om de reisinformatie voor Utrecht te laten zien
 button05 = Button(master= root,
-                  text='Actuele \n reisinformatie',
+                  text='Reisinformatie \nUtrecht',
                   bg="Blue",
                   fg= "White",
                   activebackground= "Blue",
@@ -202,7 +216,7 @@ button05.place(x=860,y=400)
 
 #button die een window opent om een Station in te voeren
 button06 = Button(master= root,
-                  text='Reisinformatie \n ander station',
+                  text='Reisinformatie \nander station',
                   bg="Blue",
                   fg= "White",
                   activebackground= "Blue",
